88f1cfc5c7d176ebdb1b2b66cb5a3f9e82d6de18
nicolasCB99 <85199855+nicolasCB99@users.noreply.github.com>
2026-02-08 21:07:47 -0500

    Assignment 2 – variables and expressions

---
+++ m2_variable_expression.py
@@ -0,0 +1,32 @@
+# -- Numeric Variables and arithmetic --
+a = 9
+b = 3
+c = 10
+d = 5
+
+total = b + c
+subtract = a - d
+product = c * d
+quotient = a / b
+
+print(f"The sum of {b} and {c} is {total}.")
+print()
+print(f"The result of {a} minus {d} is {subtract}.")
+print()
+print(f"The product of {c} and {d} is {product}.")
+print()
+print(f"The result of {a} divided by {b} is {quotient}.")
+print()
+
+# -- String variable and text manipulation --
+name = "Nicolas Castro-Bautista"
+live = "new york"
+
+print(f"Hello, my name is {name} and I live in {live.title()} state.")
+print()
+
+# -- Boolean expression and comparison --
+
+is_total_greater = total > product
+print(f"Is the sum of ({total}) greater than the product ({product})? {is_total_greater}")
+print()
